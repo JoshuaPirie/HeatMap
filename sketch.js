c1002533421e612c447a6834f2c3f8ba2da0dd33
Joshua Pirie <joshuapirie@gmail.com>
2017-01-22 23:17:01 +1030

    Reworked to improve performance

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function setup() {
   // slider setup
   heatDissipationSlider = createSlider(0, 100, 5);
   heatDissipationSlider.position(20, 40);
-  heatSpreadSlider = createSlider(1, 100, 8);
+  heatSpreadSlider = createSlider(1, 100, 20);
   heatSpreadSlider.position(20, 100);
   brushRadiusSlider = createSlider(0, 50, 3);
   brushRadiusSlider.position(20, 160);
@@ -47,13 +47,13 @@ function setup() {
   });
 
   // heatmap creation
-  heatMap = new HeatMap(gridWidthSlider.value(), gridHeightSlider.value(), cellSizeSlider.value(), cellSpacingSlider.value());
+  heatMap = new HeatMap(gridWidthSlider.value(), gridHeightSlider.value());
 }
 
 function draw() {
   // if any properties of the heatmap change, recreate it
-  if(gridWidthSlider.value() != heatMap.width || gridHeightSlider.value() != heatMap.height || cellSizeSlider.value() != heatMap.cellSize || cellSpacingSlider.value() != heatMap.cellSpacing)
-    heatMap = new HeatMap(gridWidthSlider.value(), gridHeightSlider.value(), cellSizeSlider.value(), cellSpacingSlider.value());
+  if(gridWidthSlider.value() != heatMap.width || gridHeightSlider.value() != heatMap.height)
+    heatMap = new HeatMap(gridWidthSlider.value(), gridHeightSlider.value());
 
   // redraw background for blank canvas
   background(0);
@@ -77,104 +77,98 @@ function draw() {
 }
 
 function windowResized() {
-  // resize canvas and reposition heatmap on canvas
   resizeCanvas(windowWidth, windowHeight);
-  heatMap.reposition();
 }
 
 // heatmap constructor
-var HeatMap = function(mapWidth, mapHeight, cellSize, cellSpacing) {
+var HeatMap = function(mapWidth, mapHeight) {
   this.width = mapWidth;
   this.height = mapHeight;
-  this.cellSize = cellSize;
-  this.cellSpacing = cellSpacing;
-  this.cells = [];
-  this.newCells = [];
+  this.temps = [];
+  this.newTemps = [];
 
-  // calculate top left point of where to draw heatmap from
-  var startX = (width - ((this.width - 1) * this.cellSpacing)) / 2;
-  var startY = (height - ((this.height - 1) * this.cellSpacing)) / 2;
-
-  // add a cell at each point in the grid
+  // initialize the temp at each point in the grid
   for(var x = 0; x < this.width; x++) {
-    this.cells[x] = [];
-    this.newCells[x] = [];
-    for(var y = 0; y < this.height; y++) {
-      this.cells[x][y] = new Cell(x * this.cellSpacing + startX, y * this.cellSpacing + startY);
-      this.newCells[x][y] = new Cell(x * this.cellSpacing + startX, y * this.cellSpacing + startY);
-    }
+    this.temps[x] = [];
+    this.newTemps[x] = [];
+    for(var y = 0; y < this.height; y++)
+      this.temps[x][y] = this.newTemps[x][y] = 0;
   }
 }
 
 // update function for the heatmap
 HeatMap.prototype.update = function() {
-  // take a snapshot of all the current temperatures of each cell and put them into a new array to be modified
+  // take a snapshot of all the current temperatures and put them into a new array to be modified
   for(var x = 0; x < this.width; x++)
     for(var y = 0; y < this.height; y++)
-      this.newCells[x][y].temp = this.cells[x][y].temp;
+      this.newTemps[x][y] = this.temps[x][y];
 
-  // modified the values of each cell
+  // calculate top left point of the heatmap on the screen
+  this.startX = (width - ((this.width - 1) * cellSpacingSlider.value())) / 2;
+  this.startY = (height - ((this.height - 1) * cellSpacingSlider.value())) / 2;
+
+  // modifies the values of each cell
   for(var x = 0; x < this.width; x++) {
     for(var y = 0; y < this.height; y++) {
-      // has a random chance of lowering the temperature of the cell by one
+      // has a random chance of lowering the temp by one
       if(Math.random() < heatDissipationSlider.value() / 100)
-        this.newCells[x][y].temp--;
+        this.newTemps[x][y]--;
 
       // works out how to spread the heat in a cell to adjacent cells
-      if(this.cells[x][y].temp > 0) {
+      if(this.temps[x][y] > 0) {
         // keeps track of the cells that heat can be dissipated to
         var dissipation = [];
 
         // checks all four adjacent cells to see if they are lower in temperature
-        if(this.cells[x + 1] && this.cells[x + 1][y].temp < this.cells[x][y].temp)
-          dissipation.push(this.newCells[x + 1][y]);
-        if(this.cells[x - 1] && this.cells[x - 1][y].temp < this.cells[x][y].temp)
-          dissipation.push(this.newCells[x - 1][y]);
-        if(this.cells[x][y + 1] && this.cells[x][y + 1].temp < this.cells[x][y].temp)
-          dissipation.push(this.newCells[x][y + 1]);
-        if(this.cells[x][y - 1] && this.cells[x][y - 1].temp < this.cells[x][y].temp)
-          dissipation.push(this.newCells[x][y - 1]);
+        if(this.temps[x + 1] && this.temps[x + 1][y] < this.temps[x][y])
+          dissipation.push([x + 1, y]);
+        if(this.temps[x - 1] && this.temps[x - 1][y] < this.temps[x][y])
+          dissipation.push([x - 1, y]);
+        if(this.temps[x][y + 1] < this.temps[x][y])
+          dissipation.push([x, y + 1]);
+        if(this.temps[x][y - 1] < this.temps[x][y])
+          dissipation.push([x, y - 1]);
 
         // calculates the average temperature of the cells around the current cell
         var sum = 0;
         for(var i = 0; i < dissipation.length; i++)
-          sum += dissipation[i].temp;
+          sum += this.temps[dissipation[i][0]][dissipation[i][1]];
         var average = round(sum / dissipation.length);
 
         // dissipates the heat into available cells until it either runs out of cells or the current cell has dropped below the average temp
-        while(dissipation.length > 0 && this.newCells[x][y].temp > average) {
+        while(dissipation.length > 0 && this.newTemps[x][y] > average) {
           // picks a random cell (so there's no bias if not all cells end up getting heat)
           var index = Math.floor(Math.random() * dissipation.length);
           // calculates the amount of heat to dissipate to the adjacent cell depending on the temperature difference between them
-          var amount = ceil((this.newCells[x][y].temp - dissipation[index].temp) / heatSpreadSlider.value());
+          var amount = ceil((abs(this.newTemps[x][y] - this.newTemps[dissipation[index][0]][dissipation[index][1]]) / 5) * (heatSpreadSlider.value() / 100));
           // updates cell temperatures and removes adjacent cell from the array
-          dissipation[index].temp += amount;
+          this.newTemps[dissipation[index][0]][dissipation[index][1]] += amount;
           dissipation.splice(index, 1);
-          this.newCells[x][y].temp -= amount;
+          this.newTemps[x][y] -= amount;
         }
       }
 
       // handles adding heat where the user presses
       if(mouseIsPressed) {
         // calculates the distance from the mouse to the current cell
-        var distance = dist(mouseX, mouseY, this.newCells[x][y].posX, this.newCells[x][y].posY);
+        var distance = dist(mouseX, mouseY, x * cellSpacingSlider.value() + this.startX, y * cellSpacingSlider.value() + this.startY);
         // if the cell is within a radius of the mouse, add temperature to it based on how close it is
-        if(distance < brushRadiusSlider.value() * this.cellSpacing)
-          this.newCells[x][y].temp += Math.round(map(distance, 0, brushRadiusSlider.value() * this.cellSpacing, brushIntensitySlider.value(), 0));
+        if(distance < brushRadiusSlider.value() * cellSpacingSlider.value())
+          this.newTemps[x][y] += Math.round(map(distance, 0, brushRadiusSlider.value() * cellSpacingSlider.value(), brushIntensitySlider.value(), 0));
       }
 
-      // cap the temperature to between 0 and 240
-      if(this.newCells[x][y].temp > 240)
-        this.newCells[x][y].temp = 240;
-      else if(this.newCells[x][y].temp < 0)
-        this.newCells[x][y].temp = 0;
+      // cap the temp to between 0 and 240
+      if(this.newTemps[x][y] > 240)
+        this.newTemps[x][y] = 240;
+      else if(this.newTemps[x][y] < 0)
+        this.newTemps[x][y] = 0;
     }
   }
 
-  // make the new cells the current ones and keep the old cells for use next frame
-  var temp = this.cells;
-  this.cells = this.newCells;
-  this.newCells = temp;
+  // make the new temps the current ones and keep the old temps for use next frame
+  var temp = this.temps;
+  this.temps = this.newTemps;
+  this.newTemps = temp;
 }
 
 // display function for the heatmap
@@ -191,32 +185,13 @@ HeatMap.prototype.display = function() {
   for(var x = 0; x < this.width; x++) {
     for(var y = 0; y < this.height; y++) {
       if(displayToggle) {
-        stroke(240 - this.cells[x][y].temp, 100, 100);
-        ellipse(this.cells[x][y].posX, this.cells[x][y].posY, this.cellSize);
+        stroke(240 - this.temps[x][y], 100, 100);
+        ellipse(x * cellSpacingSlider.value() + this.startX, y * cellSpacingSlider.value() + this.startY, cellSizeSlider.value());
       }
       else {
-        fill(240 - this.cells[x][y].temp, 100, 100);
-        text(this.cells[x][y].temp, this.cells[x][y].posX, this.cells[x][y].posY);
+        fill(240 - this.temps[x][y], 100, 100);
+        text(this.temps[x][y], x * cellSpacingSlider.value() + this.startX, y * cellSpacingSlider.value() + this.startY, cellSizeSlider.value());
       }
     }
   }
-}
-
-// calculates the new positions of each cell after a screen resize
-HeatMap.prototype.reposition = function() {
-  var startX = (width - ((this.width - 1) * this.cellSpacing)) / 2;
-  var startY = (height - ((this.height - 1) * this.cellSpacing)) / 2;
-  for(var x = 0; x < this.width; x++) {
-    for(var y = 0; y < this.height; y++) {
-      this.cells[x][y].posX = this.newCells[x][y].posX = x * this.cellSpacing + startX;
-      this.cells[x][y].posY = this.newCells[x][y].posY = y * this.cellSpacing + startY;
-    }
-  }
-}
-
-// cell constructor
-var Cell = function(posX, posY) {
-  this.posX = posX;
-  this.posY = posY;
-  this.temp = 0;
 }
